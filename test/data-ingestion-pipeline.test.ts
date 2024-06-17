a79f522bee0de51fafbbacb567696d36c01e0909
Sagar Panwar <sagar.panwar@dreamorbit.com>
2024-06-17 13:23:40 +0530

    project setup

---
+++ test/data-ingestion-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DataIngestionPipeline from '../lib/data-ingestion-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/data-ingestion-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DataIngestionPipeline.DataIngestionPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
